945a4b4686158c56169e9cf97fe4b8e5f9b1718b
crystallographer <r0475582@proton.me>
2023-03-26 12:36:37 +0400

    refactor: deploy script refactored

--- deploy.mjs
+++ deploy.mjs
@@ -1,14 +1,10 @@
 import { publish } from "gh-pages";
 
-function deploy() {
-    publish(
-        "dist",
-        {
-            branch: "main",
-            repo: "https://github.com/crystallographer/crystallographer.github.io.git"
-        },
-        console.error
-    );
+try {
+    publish("./dist", {
+        branch: "main",
+        repo: "https://github.com/crystallographer/crystallographer.github.io.git"
+    });
+} catch (error) {
+    console.error(error);
 }
-
-deploy();
