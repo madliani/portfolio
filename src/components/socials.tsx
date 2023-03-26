c1eb676a89147e5b49da71e1f48149ee1fc1eade
crystallographer <r0475582@proton.me>
2023-03-26 12:26:01 +0400

    fix: social component formated

--- src/components/socials.tsx
+++ src/components/socials.tsx
@@ -1,7 +1,4 @@
-import {
-    faGithub,
-    faLinkedinIn
-} from "@fortawesome/free-brands-svg-icons";
+import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
