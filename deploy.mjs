7a2a721d263a4dd7c459688a343533d610616962
crystallographer <r0475582@proton.me>
2023-04-09 00:08:01 +0400

    fix: deploy script fixed

--- deploy.mjs
+++ deploy.mjs
@@ -1,10 +1,14 @@
 import { publish } from "gh-pages";
 
 try {
-    publish("./dist", {
-        branch: "main",
-        repo: "https://github.com/crystallographer/crystallographer.github.io.git"
-    });
+    publish(
+        "./dist",
+        {
+            branch: "main",
+            repo: "https://github.com/crystallographer/crystallographer.github.io.git"
+        },
+        console.error
+    );
 } catch (error) {
     console.error(error);
 }
