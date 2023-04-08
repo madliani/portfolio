5784df47cfd56bccd15878702756bb35e690e3f8
crystallographer <r0475582@proton.me>
2023-04-08 15:32:32 +0400

    fix: background effect changed

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,8 +1,7 @@
-// eslint-disable-next-line import/default
-import ParticlesBg from "particles-bg";
-import { ReactNode } from "react";
-// eslint-disable-next-line import/default
-import MouseParticles from "react-mouse-particles";
+import { ReactNode, useCallback } from "react";
+import Particles from "react-particles";
+import { loadFull } from "tsparticles";
+import type { Engine, ISourceOptions } from "tsparticles-engine";
 
 import Navbar from "../components/navbar";
 
@@ -10,13 +9,82 @@ type Properties = {
     children: ReactNode;
 };
 
+const options: ISourceOptions = {
+    fpsLimit: 120,
+    interactivity: {
+        events: {
+            onClick: {
+                enable: true,
+                mode: "push"
+            },
+            onHover: {
+                enable: true,
+                mode: "repulse"
+            },
+            resize: true
+        },
+        modes: {
+            push: {
+                quantity: 4
+            },
+            repulse: {
+                distance: 200,
+                duration: 0.4
+            }
+        }
+    },
+    particles: {
+        color: {
+            value: "#ffffff"
+        },
+        links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1
+        },
+        collisions: {
+            enable: true
+        },
+        move: {
+            direction: "none",
+            enable: true,
+            outModes: {
+                default: "bounce"
+            },
+            random: false,
+            speed: 6,
+            straight: false
+        },
+        number: {
+            density: {
+                enable: true,
+                area: 800
+            },
+            value: 80
+        },
+        opacity: {
+            value: 0.5
+        },
+        shape: {
+            type: "circle"
+        },
+        size: {
+            value: { min: 1, max: 5 }
+        }
+    },
+    detectRetina: true
+};
+
 const Layout = ({ children }: Properties) => {
+    const init = useCallback(async (engine: Engine) => {
+        await loadFull(engine);
+    }, []);
+
     return (
         <>
-            {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
-            {/* @ts-ignore */}
-            <MouseParticles />
-            <ParticlesBg bg={true} type="cobweb" />
+            <Particles init={init} options={options} />
             <Navbar />
             <main>{children}</main>
         </>
