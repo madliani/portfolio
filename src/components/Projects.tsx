2ee3c2d3ce8d888daaa8c6390991ed6f7e5b8196
crystallographer <r0475582@proton.me>
2023-06-22 17:55:08 +0400

    fix: projects fixed

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -15,7 +15,7 @@ export const Projects = function () {
                 </div>
                 <div className="row text-center">
                     <ProjectItem
-                        imgAlt="Portfolio homepage"
+                        imgAlt="Portfolio"
                         imgSrc={PortfolioHomepage}
                         projectDescription="My personal portfolio that was built with Bootstrap, React, styled-components and TypeScript."
                         title="Portfolio"
@@ -23,20 +23,36 @@ export const Projects = function () {
                         viewSite="https://crystalographer.github.io"
                     />
                     <ProjectItem
-                        imgAlt="Reactify homepage"
+                        imgAlt="Reactify"
                         imgSrc="https://raw.githubusercontent.com/crystallographer/reactify/main/assets/images/reactify-homepage.png"
                         projectDescription="The React Starter Kit."
                         title="Reactify"
                         viewCode="https://github.com/crystallographer/reactify"
                     />
                     <ProjectItem
-                        imgAlt="Emojer icon"
+                        imgAlt="Market"
+                        imgSrc="https://raw.githubusercontent.com/crystallographer/market/main/assets/images/market-homepage.png"
+                        projectDescription="Web market."
+                        title="Market"
+                        viewCode="https://github.com/crystallographer/market"
+                    />
+                    <ProjectItem
+                        imgAlt="Emojer"
                         imgHeight="192px"
-                        imgSrc="https://raw.githubusercontent.com/crystallographer/emojer/master/assets/icons/emoji.png"
+                        imgSrc="https://raw.githubusercontent.com/crystallographer/emojer/main/assets/icons/emoji.png"
                         imgWidth="192px"
                         projectDescription="The Emoji Compiler."
                         title="Emojer"
                         viewCode="https://github.com/crystallographer/emojer"
+                    />
+                    <ProjectItem
+                        imgAlt="Caesar's Cipher"
+                        imgHeight="192px"
+                        imgSrc="https://raw.githubusercontent.com/crystallographer/caesar-cipher/main/assets/icons/key.png"
+                        imgWidth="192px"
+                        projectDescription="The Caesar's Cipher Encoder and Decoder."
+                        title="Caesar's Cipher"
+                        viewCode="https://github.com/crystallographer/caesar-cipher"
                     />
                 </div>
             </div>
