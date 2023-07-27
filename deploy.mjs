99bad0ef4e8b1bc34432459abd2521e84446bcfd
crystallographer <crystallographer@skiff.com>
2023-07-27 22:24:36 +0400

    fix: deploying script fixed

--- deploy.mjs
+++ deploy.mjs
@@ -4,7 +4,6 @@ publish(
     "./dist",
     {
         branch: "main",
-        dest: "reactify",
         repo: "https://github.com/crystallographer/crystallographer.github.io.git"
     },
     console.error
